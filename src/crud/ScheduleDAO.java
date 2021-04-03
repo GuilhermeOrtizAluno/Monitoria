ea1db04f43d21589be1c26fae0be02e04d7b5242
Ortizeira-BR <guiortizsantos12@gmail.com>
2021-04-03 00:51:10 -0300

    Create Crud

---
+++ src/crud/ScheduleDAO.java
@@ -0,0 +1,119 @@
+package crud;
+
+import bd.ConnectionFactory;
+import dice.Schedule;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+public class ScheduleDAO {
+    //Create
+    public void create(Schedule s) {
+        
+        Connection con = ConnectionFactory.getConnection();
+        
+        PreparedStatement stmt = null;
+
+        try {
+            stmt = con.prepareStatement("INSERT INTO horario_monit(horario,fk_pk_monitoria)VALUES(?,?)");
+            stmt.setDate(1, s.getHorario());
+            stmt.setInt(2, s.getFk_pk_monitoria());
+            
+            stmt.executeUpdate();
+
+            JOptionPane.showMessageDialog(null, "Salvo com sucesso!");
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        } finally {
+            ConnectionFactory.closeConnection(con, stmt);
+        }
+
+    }
+    
+    //Read
+    public List<Schedule> read(){
+
+        List<Schedule> schedules = new ArrayList<Schedule>();
+
+        Connection conn = ConnectionFactory.getConnection();
+        
+        PreparedStatement pstm = null;
+        ResultSet rset = null;
+
+        try {
+            pstm = conn.prepareStatement("SELECT * FROM horario_monit");
+
+            rset = pstm.executeQuery();
+
+            while (rset.next()) {
+
+                Schedule schedule = new Schedule();
+
+                schedule.setPk_horario_monitoria(rset.getInt("pk_horario_monitoria"));
+                schedule.setFk_pk_monitoria(rset.getInt("fk_pk_monitoria"));
+                schedule.setHorario(rset.getDate("horario"));
+
+                schedules.add(schedule);
+
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(StudentDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            ConnectionFactory.closeConnection(conn, pstm, rset);
+        }
+
+        return schedules;
+    }
+
+    //Update
+    public void update(Schedule s) {
+
+        Connection con = ConnectionFactory.getConnection();
+        
+        PreparedStatement stmt = null;
+
+        try {
+            stmt = con.prepareStatement("UPDATE horario_monit SET horario = ?, fk_pk_monitoria = ? WHERE pk_horario_monitoria = ?");
+            stmt.setDate(1, s.getHorario());
+            stmt.setInt(2, s.getFk_pk_monitoria());
+            stmt.setInt(3, s.getPk_horario_monitoria());
+
+            stmt.executeUpdate();
+
+            JOptionPane.showMessageDialog(null, "Atualizado com sucesso!");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao atualizar: " + ex);
+        } finally {
+            ConnectionFactory.closeConnection(con, stmt);
+        }
+
+    }
+    
+    //Delete
+    public void delete(Schedule s) {
+
+        Connection con = ConnectionFactory.getConnection();
+        
+        PreparedStatement stmt = null;
+
+        try {
+            stmt = con.prepareStatement("DELETE FROM horario_monit WHERE pk_horario_monitoria = ?");
+            stmt.setInt(1, s.getPk_horario_monitoria());
+
+            stmt.executeUpdate();
+
+            JOptionPane.showMessageDialog(null, "Excluido com sucesso!");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao excluir: " + ex);
+        } finally {
+            ConnectionFactory.closeConnection(con, stmt);
+        }
+
+    }
+}
