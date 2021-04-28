782c2ac142ae8b3ac0febef64609ab16c84fbae6
Ortizeira-BR <guiortizsantos12@gmail.com>
2021-04-28 20:44:26 -0300

    ...

--- Server/src/screens/Log.java
+++ Server/src/screens/Log.java
@@ -5,9 +5,14 @@
  */
 package screens;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.SwingConstants;
 
 /**
  *
@@ -56,124 +61,48 @@ public class Log extends javax.swing.JFrame {
     
     @SuppressWarnings("unchecked")                       
     private void initComponents() {
-
-        pServer = new JPanel();
+        
+        //Title
+        lTitle = new JLabel("Server");
+        lTitle.setFont(new Font("MV Boli", 1, 24)); // NOI18N
+        lTitle.setForeground(new Color(255, 255, 255));
+        lTitle.setSize(300, 500);
+        lTitle.setHorizontalAlignment(SwingConstants.CENTER);
+       
+        //Panel Title
         pTitle = new JPanel();
-        lTitle = new JLabel();
+        pTitle.setBackground(new Color(204, 0, 204));
+        pTitle.setSize(new Dimension(500, 200));
+        pTitle.add(lTitle);
+        
+        //Screens
         pContent = new JPanel();
-        spLog = new JScrollPane();
+        pContent.setBackground(new Color(255, 255, 255));
+        
+        //Panel Log
         pLog = new JPanel();
+        pLog.setBackground(new Color(204, 0, 204));
         
-        
-
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        pServer.setBackground(new java.awt.Color(255, 255, 255));
-
-        pTitle.setBackground(new java.awt.Color(204, 0, 204));
-
-        lTitle.setFont(new java.awt.Font("MV Boli", 1, 24)); // NOI18N
-        lTitle.setForeground(new java.awt.Color(255, 255, 255));
-        lTitle.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        lTitle.setText("Server");
-
-        javax.swing.GroupLayout pTitleLayout = new javax.swing.GroupLayout(pTitle);
-        pTitle.setLayout(pTitleLayout);
-        pTitleLayout.setHorizontalGroup(
-            pTitleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(lTitle, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
-        pTitleLayout.setVerticalGroup(
-            pTitleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(lTitle, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 62, Short.MAX_VALUE)
-        );
-
-        pContent.setBackground(new java.awt.Color(255, 255, 255));
-
-        spLog.setBackground(new java.awt.Color(204, 0, 204));
-
-        pLog.setBackground(new java.awt.Color(204, 0, 204));
-
-        javax.swing.GroupLayout pLogLayout = new javax.swing.GroupLayout(pLog);
-        pLog.setLayout(pLogLayout);
-        pLogLayout.setHorizontalGroup(
-            pLogLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 684, Short.MAX_VALUE)
-        );
-        pLogLayout.setVerticalGroup(
-            pLogLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 396, Short.MAX_VALUE)
-        );
-
+        //ScrollPane Log
+        spLog = new JScrollPane();
+        spLog.setBackground(new Color(204, 0, 204));
         spLog.setViewportView(pLog);
 
-        javax.swing.GroupLayout pServerLayout = new javax.swing.GroupLayout(pServer);
-        pServer.setLayout(pServerLayout);
-        pServerLayout.setHorizontalGroup(
-            pServerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(pTitle, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, pServerLayout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(pContent, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(spLog, javax.swing.GroupLayout.DEFAULT_SIZE, 703, Short.MAX_VALUE)
-                .addContainerGap())
-        );
-        pServerLayout.setVerticalGroup(
-            pServerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(pServerLayout.createSequentialGroup()
-                .addComponent(pTitle, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addGroup(pServerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(pContent, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(spLog, javax.swing.GroupLayout.DEFAULT_SIZE, 396, Short.MAX_VALUE))
-                .addContainerGap())
-        );
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(pServer, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(pServer, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
+        //Panel Main
+        pServer = new JPanel();
+        pServer.setBackground(new Color(255, 255, 255));
+        pServer.add(pTitle, BorderLayout.PAGE_START);
+        pServer.add(pContent, BorderLayout.LINE_START);
+        pServer.add(pTitle, BorderLayout.CENTER);
         
-        JPanel teste = new JPanel();
-        teste.add(new JLabel("teste"));
-        pContent.add(new JLabel("teste"));
-
+        //JFrame
+        setContentPane(pServer);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
         pack();
     }// </editor-fold>                        
 
     public void main() {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Log.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Log.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Log.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Log.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-        //</editor-fold>
-
-        /* Create and display the form */
+        
         ServerConection serverConection = new ServerConection();
         
         java.awt.EventQueue.invokeLater(new Runnable() {
